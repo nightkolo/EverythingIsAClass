6934ce78a76bc6c85d1d2b9900bd39178c5b103b
Kolo <thenitezune104@gmail.com>
2025-09-16 20:12:27 +0400

    Formatted Loops_N_Hoops

--- Loops_N_Hoops/src/loops_n_hoops/Hoops.java
+++ Loops_N_Hoops/src/loops_n_hoops/Hoops.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
  * @author kolo
  */
 public class Hoops {
+
     public static void main(String[] args) {
 //        calItemPrice(250.0, 0.5);
-        
+
 //        Scanner input = new Scanner(System.in);
-        
 //        System.out.println("Enter the radius of a circle: ");
 //        double radEntered = input.nextDouble();
 //        
@@ -23,40 +23,36 @@ public class Hoops {
 //                String.format("The area of your circle: %f",
 //                        getRadius(radEntered)
 //                ));
-
-
         Scanner input = new Scanner(System.in);
-        
+
         System.out.println("Enter price of item: ");
         double radEntered = input.nextDouble();
 //        
         calItemPrice(radEntered, 0.25);
     }
-    
+
     // Write a method that receives the radius of a circle, then calculates and returns the area of that circle.
     static public double getRadius(double radius) {
         return Math.PI * Math.pow(radius, 2);
     }
-    
+
     // Write a main method that will ask the user to input the radius,
     // calls the method above,
     //  then displays the returned value of the area.
-    
-    // Write a method that receives the price of an item, and the discount on that item,
+    // Write a method that receives the price of an item, and the discount on that item
     // then it calculates and prints the price after discount
     // and after adding tax ( a constant 5%).
     static public void calItemPrice(double price, double discount) {
         // price * (1.0 - discount_percent) * (tax_percent);
         double hiddenTaxBecauseScrewYouCustomer = 0.05;
-        
+
         double newPrice = price * (1.0 - discount) * (1.0 + hiddenTaxBecauseScrewYouCustomer);
         System.out.println(String.format("Price of your item after %.2f percent discount and %.2f percent tax: %f $",
                 discount * 100.0,
                 hiddenTaxBecauseScrewYouCustomer * 100.0,
                 newPrice));
     }
-    
+
     // In the main method, ask the user to input the price of an item,
     // then call the above method to calculate and print the final price.
-
 }
--- Loops_N_Hoops/src/loops_n_hoops/Loops.java
+++ Loops_N_Hoops/src/loops_n_hoops/Loops.java
@@ -10,23 +10,22 @@ import java.util.Scanner;
  *
  * @author kolo
  */
-
-// Exericses
-
+// Exericses...
 public class Loops {
+
     public static void main(String[] args) {
         // Write a program to display all odd numbers between 100 and 200.
         // Use a while loop
 
         int num = 100;
-        
+
         while (num <= 200) {
             if (num % 2 != 0) {
                 System.out.println(num);
             }
             num++;
         }
-        
+
         // Write a program to display all numbers between 100 and 200
         // that are divisible by 3.
         // Use a for loop
@@ -39,12 +38,9 @@ public class Loops {
             }
         }
 
-        // weakness.... not sure how to implement sentinal loops (user dependend loops)
-        
         // Write a program that will repeatedly ask the user to input a number,
         // then count all the positive and negative numbers entered by the user.
         // The program stops when the user enters a value of zero.
-        
         Scanner input = new Scanner(System.in);
 
         int posEntered = 0, negEntered = 0;
@@ -52,9 +48,10 @@ public class Loops {
         System.out.println("Input a number: ");
         int numEntered = input.nextInt();
 
+        // weakness.... not sure how to implement sentinal loops (user dependend loops)
         while (numEntered != 0) {
 //          (numEntered > 0) ? posEntered++ : negEntered++;
-            
+
             if (numEntered > 0) {
                 posEntered++;
             } else {
@@ -66,37 +63,36 @@ public class Loops {
                     numEntered));
             numEntered = input.nextInt();
         }
-
+        // got it, nevermind
         System.out.println(String.format(
                 "You have entered %d negative values, and %d positive values.",
                 negEntered,
                 posEntered
         ));
-        
+
         // Write a program that will ask the user to input the number of items purchased,
         // then for each item, asks for the item price.
         // The program then calculates and displays the total price of all items,
         // the highest price of all items, and the lowest price of all items.
-        
         Scanner input2 = new Scanner(System.in);
-        
+
         System.out.println("Enter number of items purchased: ");
         int itemsPurchased = input2.nextInt();
         double totalPrice = 0.0;
-        
-        double highest = 0.0;
-        double lowest = 9999999.0;
+
+        double highest = 0.0; // I LOVE MAGIC NUMBERS!!!!
+        double lowest = 9999999.0; // YAYYYY!!
 
         for (int i = 0; i < itemsPurchased; i++) {
             System.out.println(String.format("Enter price of item %d: ", i + 1));
             double itemPrice = input2.nextDouble();
-            
+
             totalPrice += itemPrice;
-            
+
             // I'll try the ternary operators....
             lowest = (itemPrice < lowest) ? itemPrice : lowest;
             highest = (itemPrice > highest) ? itemPrice : highest;
-            
+
 //            if (itemPrice < lowest) {
 //                lowest = itemPrice;
 //            }
@@ -104,19 +100,18 @@ public class Loops {
 //                highest = itemPrice;
 //            }
         }
-        
+
         System.out.println(
                 String.format("The total price of your %d items: %.2f",
-                itemsPurchased,
-                totalPrice
+                        itemsPurchased,
+                        totalPrice
                 )
         );
         System.out.println(
                 String.format("Highest of which: %.2f, Lowest of which : %.2f",
-                highest,
-                lowest
-        ));
-        
+                        highest,
+                        lowest
+                ));
 
     }
 }
