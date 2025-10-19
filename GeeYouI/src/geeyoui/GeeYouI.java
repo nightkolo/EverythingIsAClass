7f8d675588dc5744b9d2b752ec9cf00c14c8ad87
Kolo <thenitezune104@gmail.com>
2025-10-19 18:04:39 +0400

    Added GeeYouI
    this commit contains my totally real github password.

---
+++ GeeYouI/src/geeyoui/GeeYouI.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package geeyoui;
+
+/**
+ *
+ * @author kolo
+ */
+
+// while (infile.hasNextline())
+// infile.nextline()
+// FileReader fr
+// PrintWriter pw
+// pw.println()
+
+public class GeeYouI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
